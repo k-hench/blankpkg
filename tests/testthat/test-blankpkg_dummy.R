53ab956d7377118075bea344866b807794685785
k-hench <khench@posteo.net>
2021-03-27 20:10:07 +0100

    explicit function usage in test

--- tests/testthat/test-blankpkg_dummy.R
+++ tests/testthat/test-blankpkg_dummy.R
@@ -1,9 +1,9 @@
 library(stringr)
 
 test_that("default_str_length_is_still_17", {
-  expect_equal(stringr::str_length(blank_x()), 17)
+  expect_equal(str_length(blank_x()), 17)
 })
 
 test_that("alternaitve_inputs", {
-  expect_equal(stringr::str_length(blank_x("dadum")), 5)
+  expect_equal(str_length(blank_x("dadum")), 5)
 })
