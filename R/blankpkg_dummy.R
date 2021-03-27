413057ce42bada5195c194adefb271cd14fba74f
k-hench <khench@posteo.net>
2021-03-27 23:24:15 +0100

    adjust documentation

--- R/blankpkg_dummy.R
+++ R/blankpkg_dummy.R
@@ -1,8 +1,6 @@
 #' Dummy placeholder function
 #'
- #' @description
 #' `r lifecycle::badge("experimental")`
-#'
 #' This is a small dummy function to exemplify the
 #' {roxygen} documentation, the automatic testing etc...
 #'
